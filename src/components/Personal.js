138c57072d9b04d59ef1b81791ca8d8cf098360c
Luoxi Pan <l.panpax@gmail.com>
2023-02-13 21:32:39 +1100

    Add HTML render for Personal Details

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -3,9 +3,29 @@ import '../styles/Personal.css';
 
 class Personal extends React.Component {
     render() {
+        const loremIpsum =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+        const personalInput = (forVal, defaultVal) => (
+            <label htmlFor={forVal}>
+                <input type="text" name={forVal} defaultValue={defaultVal} />
+            </label>
+        );
+
         return (
             <div className="personal-container">
-                <div>Personal Component</div>
+                <h3>Personal Details</h3>
+                {personalInput('name', 'John Doe')}
+                {personalInput('email', 'j.doe@mail.com')}
+                <label htmlFor="number">
+                    <input type="text" name="number" defaultValue="555-555-555" />
+                </label>
+                {personalInput('location', 'Melbourne, AU')}
+                {personalInput('linkedin', 'linkedin.com/jdoe899')}
+                {personalInput('online', 'www.johndoeportfolio.com')}
+                <label htmlFor="blurb" className="personal-blurb">
+                    <textarea name="blurb" defaultValue={loremIpsum} />
+                </label>
             </div>
         );
     }

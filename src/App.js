f29e718d471d6ce14b8e8886871f49e1ca58521b
Luoxi Pan <l.panpax@gmail.com>
2023-02-16 13:22:48 +1100

    make-default-experience-global

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import WorkPreview from './components/WorkPreview';
 import Education from './components/Education';
 import './styles/App.css';
 
+const defaultWorkExperience = {
+    company: 'Acme Corp',
+    position: 'Product Tester',
+    dateFrom: 'June, 2020',
+    dateTo: 'February, 2021',
+    location: 'Acme City',
+    description: 'Survived the testing of over 100 products',
+};
+
 class App extends React.Component {
     constructor() {
         super();
@@ -20,24 +29,8 @@ class App extends React.Component {
                 online: 'www.janedoeportfolio.com',
                 blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             },
-            workDetails: {
-                company: 'Acme Corp',
-                position: 'Product Tester',
-                dateFrom: 'June, 2020',
-                dateTo: 'February, 2021',
-                location: 'Acme City',
-                description: 'Survived the testing of over 100 products',
-            },
-            workExperience: [
-                {
-                    company: 'Acme Corp',
-                    position: 'Product Tester',
-                    dateFrom: 'June, 2020',
-                    dateTo: 'February, 2021',
-                    location: 'Acme City',
-                    description: 'Survived the testing of over 100 products',
-                },
-            ],
+            workDetails: defaultWorkExperience,
+            workExperience: [defaultWorkExperience],
             workExperienceCounter: 0,
         };
 

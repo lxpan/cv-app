5d17eba4a38e92bf4ef5fceb1e8c3625c89e2a54
Luoxi Pan <l.panpax@gmail.com>
2023-02-18 22:31:45 +1100

    Refactor Education be functional component

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,44 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Education.css';
 
-class Education extends React.Component {
-    render() {
-        const { educationInfo, handleInputChange, handleEducationSubmit } = this.props;
+function Education(props) {
+    const { educationInfo, handleInputChange, handleEducationSubmit } = props;
 
-        function educationInput(forVal, value, type = 'text') {
-            return (
-                <label htmlFor={forVal}>
-                    <input
-                        type={type}
-                        name={forVal}
-                        value={value}
-                        placeholder={forVal[0].toUpperCase() + forVal.substring(1)}
-                        onChange={(event) => handleInputChange(event, 'educationDetails')}
-                    />
-                </label>
-            );
-        }
-
+    function educationInput(forVal, value, type = 'text') {
         return (
-            <div className="education-container">
-                <h3>Education Details</h3>
-                <form onSubmit={handleEducationSubmit}>
-                    {educationInput('provider', educationInfo ? educationInfo.provider : '')}
-                    {educationInput('location', educationInfo ? educationInfo.location : '')}
-                    {educationInput('program', educationInfo ? educationInfo.program : '')}
-                    {educationInput('dateFrom', educationInfo ? educationInfo.dateFrom : '')}
-                    {educationInput('dateTo', educationInfo ? educationInfo.dateTo : '')}
-                    {educationInput('major', educationInfo ? educationInfo.major : '')}
-                    {educationInput('minor', educationInfo ? educationInfo.minor : '')}
-
-                    <input type="submit" value="Add Experience" />
-                </form>
-            </div>
+            <label htmlFor={forVal}>
+                <input
+                    type={type}
+                    name={forVal}
+                    value={value}
+                    placeholder={forVal[0].toUpperCase() + forVal.substring(1)}
+                    onChange={(event) => handleInputChange(event, 'educationDetails')}
+                />
+            </label>
         );
     }
-}
 
-export default Education;
+    return (
+        <div className="education-container">
+            <h3>Education Details</h3>
+            <form onSubmit={handleEducationSubmit}>
+                {educationInput('provider', educationInfo ? educationInfo.provider : '')}
+                {educationInput('location', educationInfo ? educationInfo.location : '')}
+                {educationInput('program', educationInfo ? educationInfo.program : '')}
+                {educationInput('dateFrom', educationInfo ? educationInfo.dateFrom : '')}
+                {educationInput('dateTo', educationInfo ? educationInfo.dateTo : '')}
+                {educationInput('major', educationInfo ? educationInfo.major : '')}
+                {educationInput('minor', educationInfo ? educationInfo.minor : '')}
+
+                <input type="submit" value="Add Experience" />
+            </form>
+        </div>
+    );
+}
 
 Education.propTypes = {
     educationInfo: PropTypes.object,
@@ -47,3 +43,5 @@ Education.propTypes = {
     handleInputChange: PropTypes.func,
     handleEducationSubmit: PropTypes.func,
 };
+
+export default Education;

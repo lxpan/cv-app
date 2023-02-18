97be8fc31ff58b33c72dbe9169b74624d91f3bf2
Luoxi Pan <l.panpax@gmail.com>
2023-02-18 22:30:16 +1100

    Refactor Work to be functional component

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,48 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Work.css';
 
-class Work extends React.Component {
-    render() {
-        const { workInfo, handleInputChange, handleWorkSubmit } = this.props;
+function Work(props) {
+    const { workInfo, handleInputChange, handleWorkSubmit } = props;
 
-        function workInput(forVal, value, type = 'text') {
-            return (
-                <label htmlFor={forVal}>
-                    <input
-                        type={type}
-                        name={forVal}
-                        value={value}
-                        placeholder={forVal[0].toUpperCase() + forVal.substring(1)}
-                        onChange={(event) => handleInputChange(event, 'workDetails')}
-                    />
-                </label>
-            );
-        }
-
-        function createWorkCreatorForm() {
-            return (
-                <form onSubmit={handleWorkSubmit}>
-                    {workInput('company', workInfo ? workInfo.company : '')}
-                    {workInput('position', workInfo ? workInfo.position : '')}
-                    {workInput('dateFrom', workInfo ? workInfo.dateFrom : '')}
-                    {workInput('dateTo', workInfo ? workInfo.dateTo : '')}
-                    {workInput('location', workInfo ? workInfo.location : '')}
-                    {workInput('description', workInfo ? workInfo.description : '')}
-                    <input type="submit" value="Add Experience" />
-                </form>
-            );
-        }
-
+    function workInput(forVal, value, type = 'text') {
         return (
-            <div className="work-container">
-                <h3>Work Details</h3>
-                {createWorkCreatorForm()}
-            </div>
+            <label htmlFor={forVal}>
+                <input
+                    type={type}
+                    name={forVal}
+                    value={value}
+                    placeholder={forVal[0].toUpperCase() + forVal.substring(1)}
+                    onChange={(event) => handleInputChange(event, 'workDetails')}
+                />
+            </label>
         );
     }
-}
 
-export default Work;
+    function createWorkCreatorForm() {
+        return (
+            <form onSubmit={handleWorkSubmit}>
+                {workInput('company', workInfo ? workInfo.company : '')}
+                {workInput('position', workInfo ? workInfo.position : '')}
+                {workInput('dateFrom', workInfo ? workInfo.dateFrom : '')}
+                {workInput('dateTo', workInfo ? workInfo.dateTo : '')}
+                {workInput('location', workInfo ? workInfo.location : '')}
+                {workInput('description', workInfo ? workInfo.description : '')}
+                <input type="submit" value="Add Experience" />
+            </form>
+        );
+    }
+
+    return (
+        <div className="work-container">
+            <h3>Work Details</h3>
+            {createWorkCreatorForm()}
+        </div>
+    );
+}
 
 Work.propTypes = {
     workInfo: PropTypes.object,
@@ -51,3 +47,5 @@ Work.propTypes = {
     handleInputChange: PropTypes.func,
     handleWorkSubmit: PropTypes.func,
 };
+
+export default Work;
